a60585a46991a683d6eb7904734e9a3b18be3bf7
Will <willkang0131@gmail.com>
2023-09-27 17:33:39 -0700

    5 - Creating a form to input data

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,6 +1,7 @@
 // src/MyApp.js
 import React, {useState} from "react";
 import Table from "./Table";
+import Form from './Form';
 
 /*const characters = [
   {
@@ -23,25 +24,7 @@ import Table from "./Table";
 */
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      // the rest of the data
-      {
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        name: "Dennis",
-        job: "Bartender",
-      },
-    ]); 
+  const [characters, setCharacters] = useState([]); 
 
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
@@ -49,13 +32,13 @@ function MyApp() {
     });
   setCharacters(updated);
   }
-    
   return (
     <div className="container">
-        <Table characterData={characters} 
-          removeCharacter={removeOneCharacter} />
-    </div>  
-)
+      <Table characterData={characters} 
+        removeCharacter={removeOneCharacter} />
+      <Form />
+    </div>
+  )
 }
 
 export default MyApp;
