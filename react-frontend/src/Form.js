f1c9c5f6801301077a396ee7729894f23d951050
Will <willkang0131@gmail.com>
2023-09-27 17:39:13 -0700

    7 - Submitting form data

--- react-frontend/src/Form.js
+++ react-frontend/src/Form.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 
 
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState(
      {
         name: "",
@@ -23,6 +23,11 @@ function Form() {
        );
   }
 
+  function submitForm() {
+    props.handleSubmit(person);
+    setPerson({name: '', job: ''});
+  }
+
   return (
     <form>
       <label htmlFor="name">Name</label>
@@ -39,6 +44,7 @@ function Form() {
         id="job"
         value={person.job}
         onChange={handleChange} />
+      <input type="button" value="Submit" onClick={submitForm} />
     </form>
     );
 
